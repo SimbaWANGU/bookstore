66e88cc2eeff632a0f4e09da4da535b57bb1a551
SimbaWANGU <simbawangulu@gmail.com>
2022-06-03 15:27:17 +0300

    Complete perentage progress section

--- src/components/CompletionPercentage.js
+++ src/components/CompletionPercentage.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import '../styles/CompletionPercentage.scss';
+import CompletionPercentageCircle from './CompletionPercentageCircle';
 
 function CompletionPercentage() {
+  const percentageCompletion = 0.3;
   return (
-    <div>CompletionPercentage</div>
+    <div className="percentageCompletionContainer">
+      <CompletionPercentageCircle
+        className="circle"
+        completion={percentageCompletion}
+      />
+      <div className="numericValueContainer">
+        <h2>
+          {percentageCompletion * 100}
+          %
+        </h2>
+        <p>Completed</p>
+      </div>
+    </div>
   );
 }
 
