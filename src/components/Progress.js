1d3eaa5b9afac58ea6b65adb222749571764a64e
SimbaWANGU <simbawangulu@gmail.com>
2022-06-03 15:28:59 +0300

    Complete view books section

---
+++ src/components/Progress.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Progress.scss';
+
+function Progress() {
+  return (
+    <div className="progressContainer">
+      <p>CURRENT CHAPTER</p>
+      <h3>Chapter 17</h3>
+      <button type="button">Update Progress</button>
+    </div>
+  );
+}
+
+export default Progress;
