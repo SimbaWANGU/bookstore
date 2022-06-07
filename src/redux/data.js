e386aee6cd3d6a1d52562d2e3ee357fbaf663792
SimbaWANGU <simbawangulu@gmail.com>
2022-06-07 17:46:33 +0300

    Add dummy data as initial state

--- src/redux/data.js
+++ src/redux/data.js
@@ -1,6 +1,12 @@
 // initialState
 const state = {
   allBooks: [
+    {
+      id: 1,
+      title: 'A Game of Thrones',
+      author: 'George R.R. Martin',
+      completion: 0.32,
+    },
   ],
   allCategories: [
   ],
