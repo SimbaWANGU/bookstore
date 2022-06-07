9fab9a37d64406409b3769677984a032d2be0cb2
SimbaWANGU <simbawangulu@gmail.com>
2022-06-07 16:38:22 +0300

    Display pie chart dynamically

--- src/components/CompletionPercentage.js
+++ src/components/CompletionPercentage.js
@@ -1,18 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import '../styles/CompletionPercentage.scss';
 import CompletionPercentageCircle from './CompletionPercentageCircle';
 
-function CompletionPercentage() {
-  const percentageCompletion = 0.3;
+function CompletionPercentage({ completion }) {
+  // const percentageCompletion = 0.3;
   return (
     <div className="percentageCompletionContainer">
       <CompletionPercentageCircle
         className="circle"
-        completion={percentageCompletion}
+        completion={completion}
       />
       <div className="numericValueContainer">
         <h2>
-          {percentageCompletion * 100}
+          {completion * 100}
           %
         </h2>
         <p>Completed</p>
