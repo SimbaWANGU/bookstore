2fd6fa066864768ef06c5f676e5efe061a070fc9
SimbaWANGU <simbawangulu@gmail.com>
2022-06-07 16:32:56 +0300

    Refactor categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,21 +1,22 @@
 // Actions
 const CHECK_STATUS = 'bookstore/category/CHECK_STATUS';
 
-// Reducer
-export default function categoryReducer(state = [], action = {}) {
-  switch (action.type) {
-    case CHECK_STATUS: {
-      return 'Under Construction';
-    }
-    default: {
-      return state;
-    }
-  }
-}
-
 // Action Creators
 export function checkStatus() {
   return {
     type: CHECK_STATUS,
   };
+}
+
+// Reducer
+const initialCategories = [];
+export default function categoryReducer(allCategories = initialCategories, action) {
+  switch (action.type) {
+    case CHECK_STATUS: {
+      return 'Under Construction';
+    }
+    default: {
+      return allCategories;
+    }
+  }
 }
