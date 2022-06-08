f0a51fad8c0483b177874c215f1cdc823d33dd5c
SimbaWANGU <simbawangulu@gmail.com>
2022-06-08 16:29:02 +0300

    Edit store configurations

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,16 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import allBooksReducer from './books/books';
 import categoryReducer from './categories/categories';
-import books from './data';
 
-const reducers = combineReducers({
+const allReducers = combineReducers({
   allBooks: allBooksReducer,
   allCategories: categoryReducer,
 });
 
 const store = createStore(
-  (state, action) => reducers(state, action),
-  books,
+  allReducers,
+  applyMiddleware(thunk),
 );
 
 export default store;
